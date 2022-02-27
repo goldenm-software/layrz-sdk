8f6b15a43f608771eca9ad76b7e123f780ec376a
Kenny Mochizuki <kenny.mochizuki@gmail.com>
2022-02-27 16:06:22 -0600

    Added Range charts entities

---
+++ CHANGELOG.rst
@@ -0,0 +1,18 @@
+=========
+Changelog
+=========
+
+v1.0.2
+~~~~~~
+* Added entities for Range Charts:
+  - Line Charts
+  - Area Charts
+  - Column Charts
+
+v1.0.1
+~~~~~~
+* Added entities for Sensors and Triggers
+
+v1.0.0
+~~~~~~
+* Initial release
--- layrzsdk/entities/__init__.py
+++ layrzsdk/entities/__init__.py
@@ -3,3 +3,5 @@ from .asset import Asset, Sensor, AssetOperationMode
 from .device import Device
 from .custom_field import CustomField
 from .mesage import Position, Message
+from .range_charts import LineChart, AreaChart, ColumnChart
+from .charts import ChartDataSerie, ChartAlignment, ChartException
