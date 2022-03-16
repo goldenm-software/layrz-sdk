58c78011371e6fc3cc030a08961e1ffec369ecd2
Kenny Mochizuki <kenny.mochizuki@gmail.com>
2022-03-16 13:39:59 -0600

    feature/New BroadcastStatus value

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(filename):
 
 setuptools.setup(
   name="layrz-sdk",
-  version="1.2.2",
+  version="1.2.3",
   author="Layrz",
   author_email="software@layrz.com",
   url='https://gitlab.com/layrz-software/libraries/layrz-sdk',
