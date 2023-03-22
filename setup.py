0ace9c06642bf9a07b449dbaaf633f976483df80
Kenny Mochizuki <kenny.mochizuki@gmail.com>
2023-03-22 16:39:14 -0600

    Unified namespace

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+""" Setup file for layrz-sdk. """
 import setuptools
 
 
 def read(filename):
   import os
-  return open(os.path.join(os.path.dirname(__file__), filename)).read()
+  return open(os.path.join(os.path.dirname(__file__), filename), 'r', encoding='utf-8').read()
 
+def get_requirements():
+  with open('requirements.txt', 'rb') as f:
+    lines = f.read().decode('utf-8').splitlines()
+
+  return [line for line in lines if not line.startswith('--')]
 
 setuptools.setup(
   name="layrz-sdk",
-  version="1.4.3",
+  version="2.0.0",
   author="Layrz",
   author_email="software@layrz.com",
   url='https://gitlab.com/layrz-software/libraries/layrz-sdk',
@@ -22,6 +28,6 @@ setuptools.setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  install_requires=['requests', 'pytz', 'xlsxwriter'],
+  install_requires=get_requirements(),
   python_requires='>=3.8',
 )
