976b9d35076390578fa0dc0d387dda045e1a9264
KasenLi <kasenkelvin.10@gmail.com>
2023-03-24 18:04:04 -0400

    Upgrade version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setuptools.setup(
   name="layrz-sdk",
-  version="1.4.3",
+  version="1.4.4",
   author="Layrz",
   author_email="software@layrz.com",
   url='https://gitlab.com/layrz-software/libraries/layrz-sdk',
