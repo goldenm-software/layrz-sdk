4a8d5a5c796e2c774b5aaecf7ae6a0f8ef61c67c
Kenny Mochizuki <kenny@goldenm.com>
2024-07-17 02:10:17 -0600

    Changes required to support more event information

--- layrz_sdk/entities/__init__.py
+++ layrz_sdk/entities/__init__.py
@@ -1,4 +1,5 @@
-""" Init file """
+"""Init file"""
+
 # Broadcast entities
 from .broadcasts import BroadcastRequest, BroadcastResponse, BroadcastResult, BroadcastStatus, OutboundService
 
@@ -7,41 +8,41 @@ from .cases import Case, CaseIgnoredStatus, CaseStatus, Comment, Trigger
 
 # Charts entities
 from .charts import (
-                     AreaChart,
-                     AxisConfig,
-                     BarChart,
-                     ChartAlignment,
-                     ChartColor,
-                     ChartConfiguration,
-                     ChartDataSerie,
-                     ChartDataSerieType,
-                     ChartDataType,
-                     ChartException,
-                     ChartRenderTechnology,
-                     ColumnChart,
-                     HTMLChart,
-                     LineChart,
-                     MapCenterType,
-                     MapChart,
-                     MapPoint,
-                     NumberChart,
-                     PieChart,
-                     RadarChart,
-                     RadialBarChart,
-                     ScatterChart,
-                     ScatterSerie,
-                     ScatterSerieItem,
-                     TableChart,
-                     TableHeader,
-                     TableRow,
-                     TimelineChart,
-                     TimelineSerie,
-                     TimelineSerieItem,
-                     get_color_list,
+  AreaChart,
+  AxisConfig,
+  BarChart,
+  ChartAlignment,
+  ChartColor,
+  ChartConfiguration,
+  ChartDataSerie,
+  ChartDataSerieType,
+  ChartDataType,
+  ChartException,
+  ChartRenderTechnology,
+  ColumnChart,
+  HTMLChart,
+  LineChart,
+  MapCenterType,
+  MapChart,
+  MapPoint,
+  NumberChart,
+  PieChart,
+  RadarChart,
+  RadialBarChart,
+  ScatterChart,
+  ScatterSerie,
+  ScatterSerieItem,
+  TableChart,
+  TableHeader,
+  TableRow,
+  TimelineChart,
+  TimelineSerie,
+  TimelineSerieItem,
+  get_color_list,
 )
 
 # Checkpoints entities
-from .checkpoints import Checkpoint, Geofence, Waypoint
+from .checkpoints import Checkpoint, Waypoint
 
 # Events entities
 from .events import Event
@@ -50,22 +51,22 @@ from .events import Event
 from .formatting import TextAlignment
 
 # General entities
-from .general import Asset, AssetOperationMode, CustomField, Device, Sensor, User
+from .general import Asset, AssetOperationMode, CustomField, Device, Geofence, PresenceType, Sensor, User
 
 # REPCOM entities
 from .repcom import Transaction
 
 # Reports entities
 from .reports import (
-                     CustomReportPage,
-                     Report,
-                     ReportCol,
-                     ReportConfiguration,
-                     ReportDataType,
-                     ReportFormat,
-                     ReportHeader,
-                     ReportPage,
-                     ReportRow,
+  CustomReportPage,
+  Report,
+  ReportCol,
+  ReportConfiguration,
+  ReportDataType,
+  ReportFormat,
+  ReportHeader,
+  ReportPage,
+  ReportRow,
 )
 
 # Telemetry entities
--- layrz_sdk/entities/events/event.py
+++ layrz_sdk/entities/events/event.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 
 from layrz_sdk.entities.cases.trigger import Trigger
+from layrz_sdk.entities.general.geofence import Geofence, PresenceType
 from layrz_sdk.entities.telemetry.message import Message
 
 
@@ -17,6 +18,8 @@ class Event:
     asset_id : ID of the Asset owner of the event
     message : Telemetry information of the event
     activated_at : Reception/triggered at
+    geofence : Geofence where the event occurred
+    presence_type : Presence type of the event
   """
 
   def __init__(
@@ -26,6 +29,8 @@ class Event:
     asset_id: int,
     message: Message,
     activated_at: datetime,
+    geofence: Geofence | None = None,
+    presence_type: PresenceType | None = None,
   ) -> None:
     """Constructor"""
     self.pk = pk
@@ -33,6 +38,8 @@ class Event:
     self.asset_id = asset_id
     self.message = message
     self.activated_at = activated_at
+    self.geofence = geofence
+    self.presence_type = presence_type
 
   @property
   def _readable(self) -> str | None | bool:
