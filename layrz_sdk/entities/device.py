b01667673036a354cc788df6f6e890b975a69ff8
Kenny Mochizuki <kenny@goldenm.com>
2025-06-30 15:20:56 -0600

    Fix

--- layrz_sdk/entities/device.py
+++ layrz_sdk/entities/device.py
@@ -11,7 +11,10 @@ class Device(BaseModel):
   pk: int = Field(description='Defines the primary key of the device')
   name: str = Field(description='Defines the name of the device')
   ident: str = Field(description='Defines the identifier of the device')
-  protocol_id: int | None = Field(description='Defines the protocol ID of the device')
+  protocol_id: int | None = Field(
+    description='Defines the protocol ID of the device',
+    default=None,
+  )
   protocol: str = Field(description='Defines the protocol of the device')
   is_primary: bool = Field(default=False, description='Defines if the device is the primary device')
 
